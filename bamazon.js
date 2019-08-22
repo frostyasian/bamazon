b96186561a5727046d1f1405853beb995b9392c0
frostyasian <zphang@gmail.com>
2019-08-22 15:30:26 -0400

    Finishing up Customer

--- bamazon.js
+++ bamazon.js
@@ -1,7 +1,6 @@
-let databaseName = "bamazon_db";
-let tableName = "products";
-///////////////////////////////
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+var databaseName = "bamazon_db";
 var connection = mysql.createConnection({
   host: "localhost",
   port: 8889,
@@ -9,20 +8,119 @@ var connection = mysql.createConnection({
   password: "root",
   database: databaseName
 });
-//RUN PROGRAM//
+
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  showAllItems();
+  console.log("connected as id " + connection.threadId + "\n");
 });
-//PRODUCT FUNCTIONS//
-function pickingAnItem(product_name) {
-  connection.query(
-    `SELECT product_name FROM ${tableName} WHERE product_name='${product_name}'`,
-    function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      let stock_quantity;
-    }
-  );
+function userPurchasePrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "item_id",
+        message: "Enter the item ID of the product you would like to purchase.",
+        filter: Number
+      },
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many of the product would you like to purchase?",
+        filter: Number
+      }
+    ])
+    .then(function(input) {
+      var item = input.item_id;
+      var quantity = input.quantity;
+
+      var queryStr = `SELECT * FROM products WHERE ?`;
+
+      connection.query(queryStr, { item_id: item }, function(err, data) {
+        if (err) throw err;
+
+        if (data.length === 0) {
+          console.log("ERROR: Invalid Item ID. Please enter a valid Item ID.");
+          displayInventory();
+        } else {
+          var productData = data[0];
+          if (quantity <= productData.stock_quantity) {
+            console.log(
+              "==================================================================================="
+            );
+            console.log(
+              "Congratulations, the product you have requested is in stock! Placing order!"
+            );
+
+            var updateQueryStr =
+              "UPDATE products SET stock_quantity = " +
+              (productData.stock_quantity - quantity) +
+              " WHERE item_id = " +
+              item;
+            connection.query(updateQueryStr, function(err, data) {
+              if (err) throw err;
+              console.log(
+                "Your order has been placed! Your total $" +
+                  productData.price * quantity
+              );
+              console.log("Thank you for shopping with us! Good-Bye!");
+              console.log(
+                "==================================================================================="
+              );
+              connection.end();
+            });
+          } else {
+            console.log(
+              "Sorry, there was not enough product in stock, your order cannot be placed as is."
+            );
+            console.log("Please modify your order.");
+            console.log(
+              "==================================================================================="
+            );
+            displayInventory();
+          }
+        }
+      });
+    });
 }
+function displayInventory() {
+  queryStr = "SELECT * FROM products";
+
+  connection.query(queryStr, function(err, data) {
+    if (err) throw err;
+
+    console.log(
+      "Welcome to Bamazon! We have just what you may need in our traveling market! Here is our Existing Inventory: "
+    );
+
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      console.log(
+        "==================================================================================="
+      );
+      console.log(
+        `|NAME \t         | ID \t |DEPARTMENT \t | PRICE: \t| STOCK QUANTITY: \t|`
+      );
+      console.log(
+        "==================================================================================="
+      );
+      for (i = 0; i < res.length; i++) {
+        console.log(
+          `${res[i].product_name} \t | ${res[i].item_id} \t | ${
+            res[i].department_name
+          } \t | ${res[i].price} \t        | ${res[i].stock_quantity}`
+        );
+      }
+      console.log(
+        "==================================================================================="
+      );
+
+      userPurchasePrompt();
+    });
+  });
+}
+
+function runBamazon() {
+  displayInventory();
+}
+
+runBamazon();
--- bamazon.sql
+++ bamazon.sql
@@ -1,23 +1,31 @@
 CREATE DATABASE bamazon_db;
 USE bamazon_db;
-CREATE TABLE products (
-item_id INTEGER NOT NULL AUTO_INCREMENT,
-product_name VARCHAR(100) NOT NULL,
-department_name VARCHAR(100) NOT NULL,
+CREATE TABLE products
+(
+      item_id INTEGER NOT NULL
+      AUTO_INCREMENT,
+product_name VARCHAR
+      (100) NOT NULL,
+department_name VARCHAR
+      (100) NOT NULL,
 price INTEGER default 0,
 stock_quantity INTEGER default 0,
-PRIMARY KEY(item_id)
+PRIMARY KEY
+      (item_id)
 );
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("1", "Macbook Pro", "Electronics", 1500, 3),
-      ("2", "Nintendo Switch", "Electronics", 400, 6),
-      ("3", "IKEA Couch", "Furniture", 1000, 5),
-      ("4", "Canada Goose", "Clothing", 800, 1),
-      ("5", "Skateboard", "Toys", 100, 4),
-      ("6", "Dining Room Set", "Furniture", 750, 2),
-      ("7", "Framed Abstract Wall Art", "Home Decor", 20, 7),
-      ("8", "Round Wall Mirror", "Home Decor", 12, 10),
-      ("9", "Queen IKEA Bed", "Furniture", 600, 1),
-      ("10", "IKEA Dresser", "Furniture", 200, 2);
+      INSERT INTO products
+            (item_id, product_name, department_name, price, stock_quantity)
+      VALUES
+            ("1", "Macbook Pro", "Electronics", 150, 3),
+            ("2", "Playstation", "Electronics", 400, 5),
+            ("3", "IKEA Couch", "Furniture", 100, 1),
+            ("4", "Canada Goose", "Clothing", 800, 1),
+            ("5", "Skateboard", "Toys", 100, 10),
+            ("6", "Dining Table", "Furniture", 40, 2),
+            ("7", "Framed Art", "Home Decor", 20, 6),
+            ("8", "Round Mirror", "Home Decor", 12, 4),
+            ("9", "Queen IKEA Bed", "Furniture", 600, 1),
+            ("10", "IKEA Dresser", "Furniture", 200, 2);
 
-SELECT * FROM products;
+      SELECT *
+      FROM products;
